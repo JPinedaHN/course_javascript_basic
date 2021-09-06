f9777b13f633bd490b5918b42019e917b43f5e30
Joel Pineda <devs.jpineda@gmail.com>
2021-09-06 16:13:32 -0600

    1. Values types

---
+++ 1_valores.js
@@ -0,0 +1,28 @@
+// TIPOS PRIMITIVOS
+//tipo numero o "number"
+40
+
+// tipo cadena de texto o "string", Esto es la forma en la que puedo crear algo que se pueda imprimir en la pantalla, o escribir información, que el usuario pueda entender y leer. utilizar palabras, o frases.
+"Joel Pineda"
+'Joel Pineda'
+
+//tipo booleano o "boolean"
+true   
+false
+
+// Valores vacios (Empty values), es como un placeholder (espacio reservado para algun valor) de algún valor faltante. 
+null
+undefined
+
+ /* ================================================================ */ 
+// Tipos de Objetos o NO PRIMITIMOS:
+//Tipo array que va con []
+[1,2,3]
+
+//Tipo objeto que va con {}
+{nombre: "Joel Pineda"}
+
+ /* ================================================================ */ 
+
+//Verificamos que son de un tipo con el typeof()
+typeof(40) //retorna "number" y asi para cada uno.
