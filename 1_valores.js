e72eac8bbc2215cd1cf0b2cfd8a61591658858d6
Joel Pineda <devs.jpineda@gmail.com>
2021-09-06 16:36:04 -0600

    Variables

--- 1_valores.js
+++ 1_valores.js
@@ -3,8 +3,8 @@
 40
 
 // tipo cadena de texto o "string", Esto es la forma en la que puedo crear algo que se pueda imprimir en la pantalla, o escribir información, que el usuario pueda entender y leer. utilizar palabras, o frases.
-"Joel Pineda"
-'Joel Pineda'
+"Pedro Picapiedra"
+'Pedro Picapiedra'
 
 //tipo booleano o "boolean"
 true   
@@ -20,9 +20,31 @@ undefined
 [1,2,3]
 
 //Tipo objeto que va con {}
-{nombre: "Joel Pineda"}
+{nombre: "Pedro Picapiedra"}
 
  /* ================================================================ */ 
 
 //Verificamos que son de un tipo con el typeof()
 typeof(40) //retorna "number" y asi para cada uno.
+
+
+//En resumen los tipos son:
+Number 
+Boolean
+String
+undefined
+null
+Object
+Symbol
+
+// typeof es un tipo de operador unitario "Unary operator" que trabaja con 1 operando (variable)
+
+var nombre; 
+
+typeof 30 
+typeof true
+typeof "Diego" 
+typeof nombre 
+typeof null
+typeof {}
+typeof []
---
+++ 2_variables.js
@@ -0,0 +1,21 @@
+var nombre = 'Pedro';
+
+//declarar
+var edad;
+// edad es undefined
+
+//inicializar
+edad = 30;
+// edad es tipo number
+
+//declarar e inicializar con el "="
+var elementos = ["Computadora", "Celular"];
+//elementos es tipo String[], un array de strings
+
+var persona = {
+  nombre: 'Pedro',
+  edad: 30
+}
+//persona es de tipo object {nombre:string, edad:number}
+
+
